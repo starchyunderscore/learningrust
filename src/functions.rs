bb118a49b600bc561e47f6b4deb5bbe805495f5c
starchyunderscore <starchy_underscore@gmx.com>
2023-02-22 22:01:09 -0500

    added is_even, as one does

--- src/functions.rs
+++ src/functions.rs
@@ -1,6 +1,8 @@
 pub fn run() {
     println!("{}",double(8));
     println!("{}", complex(5));
+    let a_number:i32=8;
+    println!("{} is even? {}",a_number,is_even(a_number))
 }
 
 fn double(input:i32) -> i32 {
@@ -11,4 +13,8 @@ fn complex(input:i32) -> i32 {
     let doubled = double(input);
     let halved = input/2;
     doubled - halved
+}
+
+fn is_even(num:i32) -> bool {
+    num%2==0
 }
