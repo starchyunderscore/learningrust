e973f7dd20ee96d1be42e22d2c21fc6fbf643d45
starchyunderscore <starchy_underscore@gmx.com>
2023-02-22 21:56:17 -0500

    added a wack function because i wanted to. its useless but whatever

--- src/functions.rs
+++ src/functions.rs
@@ -1,7 +1,14 @@
 pub fn run() {
     println!("{}",double(8));
+    println!("{}", complex(5));
 }
 
 fn double(input:i32) -> i32 {
     input * 2
+}
+
+fn complex(input:i32) -> i32 {
+    let doubled = double(input);
+    let halved = input/2;
+    doubled - halved
 }
