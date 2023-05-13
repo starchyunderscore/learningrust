42fcf55055e9399ae5f314d2ab21491c356978ca
starchyunderscore <starchy_underscore@gmx.com>
2023-05-13 15:32:52 -0400

    update uinput.rs

--- src/uinput.rs
+++ src/uinput.rs
@@ -1,9 +1,20 @@
 pub fn run () {
+    println!("Write somethnig: ");
     println!("You wrote: {}", read_input().trim());
+    if continu() {
+        println!("e");
+    }
 }
 
 fn read_input() -> String {
-    let mut input: String = String::new();
+    let mut input:String = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
     input
-} 
+}
+
+fn continu() -> bool {
+    println!("continue? (y/n)");
+    let mut input:String = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    input.trim() == "y"
+}
